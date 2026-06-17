732d636083378a48c5efe7d66a06264af312be2b
Ubuntu <ubuntu@ip-172-31-26-31.ec2.internal>
2026-06-17 18:44:18 +0000

    Docker files of both backend and frontend  in working condition

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,18 @@
 import mongoose from 'mongoose';
-import { MONGODB_URI } from './utils.js';
-export default function connectDB() {
+
+const connectDB = async () => {
   try {
-    mongoose.connect(MONGODB_URI);
-  } catch (err) {
-    console.error(err.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
+};
 
-  const dbConnection = mongoose.connection;
 
-  dbConnection.once('open', () => {
-    console.log(`Database connected: ${MONGODB_URI}`);
-  });
 
-  dbConnection.on('error', (err) => {
-    console.error(`connection error: ${MONGODB_URI}`);
-  });
-  return;
-}
+export default connectDB;
