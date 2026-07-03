ef70b1c9d2b1e3f27822899a19975049436ead3f
Roshan <learnwithroshanmishra@gmail.com>
2026-07-03 05:30:51 +0000

    Docker multi stage and some modifications added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,15 @@
 import mongoose from 'mongoose';
+import { MONGODB_URI } from './utils.js';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(MONGODB_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
-
-
 
 export default connectDB;
